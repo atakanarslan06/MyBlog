1a169da3d041c7656e010a21353f310642d1209a
Atakan Arslan <atak.arslan@hotmail.com>
2022-12-20 17:17:32 +0300

    Map islemleri yapıldı

---
+++ Blog.Service/AutoMapper/Artices/ArticleProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Blog.Entity.DTOs.Articles;
+using Blog.Entity.Entities;
+
+namespace Blog.Service.AutoMapper.Artices
+{
+    public class ArticleProfile : Profile
+    {
+        public ArticleProfile()
+        {
+            CreateMap<ArticleDto, Article>().ReverseMap();
+        }
+    }
+}
